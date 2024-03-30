ba831bb0ebf5ea8a38417e3d2b62adee9bcd1885
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-03-30 00:41:33 +0000

    Fully operating setValue

--- app/src/main/java/com/example/firebaseapp/MainActivity.kt
+++ app/src/main/java/com/example/firebaseapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.firebaseapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //RealTime db Reference
+        //https://fir-kotlin-bff91-default-rtdb.europe-west1.firebasedatabase.app/
+        database = Firebase.database("https://fir-kotlin-bff91-default-rtdb.europe-west1.firebasedatabase.app").reference
+
+        //Write data  to Firebase
+        database.child("price").setValue("12312 $")
+
     }
 }
