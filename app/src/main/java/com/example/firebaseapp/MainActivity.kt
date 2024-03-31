affac8c30c24a5a03df3f775d080b5bda0570bb8
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-03-31 11:04:48 +0100

    Reading and Writting simple values on real time database

--- app/src/main/java/com/example/firebaseapp/MainActivity.kt
+++ app/src/main/java/com/example/firebaseapp/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.firebaseapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
@@ -15,13 +19,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val textView: TextView = findViewById(R.id.textView)
 
+
+        /**
         //RealTime db Reference
         //https://fir-kotlin-bff91-default-rtdb.europe-west1.firebasedatabase.app/
         database = Firebase.database("https://fir-kotlin-bff91-default-rtdb.europe-west1.firebasedatabase.app").reference
 
         //Write data  to Firebase
         database.child("price").setValue("12312 $")
+
+        //reading data
+        val postListener = object  : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val gold_price = snapshot.value
+
+                textView.text = gold_price.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        }
+
+        database.child("price").addValueEventListener(postListener)
+        **/
 
     }
 }
